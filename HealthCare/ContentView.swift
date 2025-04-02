7278ee8af8c0da92fcea6818bcb69c15382377dc
imhalid <imhalid@icloud.com>
2025-04-02 16:00:41 +0300

    Initial Commit

---
+++ HealthCare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthCare
+//
+//  Created by Halit İçli on 2.04.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthCare/HealthCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthCareApp.swift
+//  HealthCare
+//
+//  Created by Halit İçli on 2.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct HealthCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
